379730b8c78cad8cf4a282b602f9a916e2af91ec
Zayd Khan <khanzayd11@gmail.com>
2026-04-14 12:40:22 -0500

    basic generate + test loop working

---
+++ human_eval.py
@@ -0,0 +1,87 @@
+import json
+import requests
+
+# TODO: move these to a config file or smth
+LM_STUDIO_HOST = "localhost"
+LM_STUDIO_PORT = 1234
+
+SYSTEM_PROMPT = "You are a coding assistant. Respond ONLY in the request json format. Do not include imports. Do not include additional test cases. Do not include backticks"
+IMPORTS = "from typing import List\n\n"
+
+def generate_one_solution(prompt):
+  url = f"http://{LM_STUDIO_HOST}:{LM_STUDIO_PORT}/v1/chat/completions"
+
+  headers = {
+     "Content-Type": "application/json"
+  }
+
+  data = {
+     "messages": [
+        {"role": "system", "content": SYSTEM_PROMPT},
+        {"role": "user", "content": prompt}
+     ],
+     "response_format": {
+        "type": "json_schema",
+        "json_schema": {
+           "name": "code_repsonse",
+           "strict": True,
+           "schema": {
+              "type": "object",
+              "properties": {
+                 "code": {
+                    "type": "string"
+                 }
+              },
+              "required": ["joke"]
+           }
+        }
+     },
+     "temperature": 0.7,
+     "max_tokens": -1,
+     "stream": False
+  }
+
+  try:
+     response = requests.post(url, headers=headers, data=json.dumps(data), timeout=60)
+     response.raise_for_status()
+
+     result = response.json()
+     return json.loads(result['choices'][0]['message']['content'])["code"]
+  except requests.exceptions.RequestException as e:
+     print(e)
+     return None
+
+question_path = "HumanEval.jsonl"
+data = []
+
+
+
+with open(question_path, 'r') as file:
+  for line in file:
+      data.append(json.loads(line))
+
+for d in data[:3]:
+   prompt = d["prompt"]
+   test = d["test"]
+   function_name = d["entry_point"]
+
+   # send the prompt to LMStudio
+   completion = generate_one_solution(prompt)
+
+   codestring = f"{IMPORTS}{completion}{test}\n\ncheck({function_name})"
+   print(codestring)
+
+   exec(codestring)
+
+   # My guess is: we have to stitch the prompt and completion together?
+   # As well as the included tests, so we have a string that we can pass into exec. Fun!
+   # 
+
+   
+
+   # write the solution to some form of external storage / some queue for processing
+   # or, just a file for now :) will probably have to run the solutions inside of docker.
+
+
+
+
